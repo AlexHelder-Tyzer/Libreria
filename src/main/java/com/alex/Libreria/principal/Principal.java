21cf90fe69d655983144d7c2dcebc908a1cb998e
tyzer <helder.huancara@gmail.com>
2024-06-27 22:40:28 -0500

    Modulo de Top de 10 libros mas descargados

--- src/main/java/com/alex/Libreria/principal/Principal.java
+++ src/main/java/com/alex/Libreria/principal/Principal.java
@@ -1,8 +1,12 @@
 package com.alex.Libreria.principal;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import com.alex.Libreria.model.Datos;
+import com.alex.Libreria.model.DatosLibros;
 import com.alex.Libreria.service.ConsumoAPI;
 import com.alex.Libreria.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -15,5 +19,15 @@ public class Principal {
 
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        // Top 10 libros mas descargados
+        System.out.println("Top 10 libros mas buscados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
+
+
 }
