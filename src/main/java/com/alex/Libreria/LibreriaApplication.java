83baf8a7d94e5b2f6d48cf9037080d9092ccefd0
tyzer <helder.huancara@gmail.com>
2024-06-27 22:34:50 -0500

    Modelamiento de clases

--- src/main/java/com/alex/Libreria/LibreriaApplication.java
+++ src/main/java/com/alex/Libreria/LibreriaApplication.java
@@ -1,13 +1,20 @@
 package com.alex.Libreria;
 
+import com.alex.Libreria.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibreriaApplication {
+public class LibreriaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+	}
 }
---
+++ src/main/java/com/alex/Libreria/principal/Principal.java
@@ -0,0 +1,19 @@
+package com.alex.Libreria.principal;
+
+import com.alex.Libreria.model.Datos;
+import com.alex.Libreria.service.ConsumoAPI;
+import com.alex.Libreria.service.ConvierteDatos;
+
+public class Principal {
+    private static final String URL_BASE = "https://gutendex.com/books/";
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraElMenu(){
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        System.out.println(json);
+
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+    }
+}
