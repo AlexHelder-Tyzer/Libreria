a30550ec7c0abe8a7d1c06b01e7f8bbfda81501a
tyzer <helder.huancara@gmail.com>
2024-06-27 22:59:21 -0500

    Modulo de busqueda de libros por nombre

--- src/main/java/com/alex/Libreria/principal/Principal.java
+++ src/main/java/com/alex/Libreria/principal/Principal.java
@@ -7,11 +7,14 @@ import com.alex.Libreria.service.ConsumoAPI;
 import com.alex.Libreria.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
+    private Scanner teclado = new Scanner(System.in);
 
     public void muestraElMenu(){
         var json = consumoAPI.obtenerDatos(URL_BASE);
@@ -27,6 +30,24 @@ public class Principal {
                 .limit(10)
                 .map(l -> l.titulo().toUpperCase())
                 .forEach(System.out::println);
+
+        //Busqueda de libros por nombre
+        System.out.println("Ingrese el libro que desea buscar: ");
+        var tituloLibro = teclado.nextLine();
+        json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + tituloLibro.replace(" ","+"));
+        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+
+        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+        if(libroBuscado.isPresent()){
+            System.out.println("Libro encontrado!!");
+            System.out.println(libroBuscado.get()); // trae todos los datos el libro
+        }
+        else{
+            System.out.println("Libro no encontrado!!");
+        }
+
     }
 
 
